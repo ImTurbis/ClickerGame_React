cb1b2ea56744c3aa8da681f26b976d381934d36f
ElTurbinas <73681786+ElTurbinas@users.noreply.github.com>
2022-06-18 16:57:27 +0000

    Clicks per second repair

--- pages/api/local/game.js
+++ pages/api/local/game.js
@@ -1,12 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { NextApiRequest, NextApiResponse } from "next";
-
 /**
  * 
  * @param {NextApiRequest} req 
  * @param {NextApiResponse} res 
  */
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    res.status(200).json(JSON.parse(localStorage.getItem("ClickerGame")));
 }
--- pages/game.jsx
+++ pages/game.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import styles from '../styles/Game.module.css';
 import Head from 'next/head';
+import axios from 'axios';
+
+
 
 export default function Game() {
     const alias = useRef();
     var seconds = 0;
+
+    //Obtener ip
+
 
     const [game, todoGame] = useState({
         game: 'ClickerGame',
@@ -14,7 +21,22 @@ export default function Game() {
         clicksPerSecond: 0,
         required: 1000,
         clicks: 0,
+        seconds: 0,
     })
+
+    useEffect(() => {
+            localStorage.setItem(game.game, JSON.stringify(game));
+        }, [game]
+    )
+
+    useEffect(() => {
+        const localStorageGame = JSON.parse(localStorage.getItem(game.game));
+        if(!localStorageGame) return alert('No hay nada en el localStorage');
+        todoGame(() => {return {...localStorageGame}});
+        console.log('Data Cargada')
+        isLoad = true;
+    }, [])
+
 
     var points = game.points;
     var wins = game.wins;
@@ -22,43 +44,65 @@ export default function Game() {
     var required = game.required;
     var clicks = game.clicks;
     var id = game.id;
+    var isLoad = false
 
-    var gameObject = {
-        game: 'ClickerGame',
-        id: id,
-        points: points,
-        wins: wins,
-        clicksPerSecond: clicksPerSecond,
-        required: required,
-        clicks: clicks,
-    }
-
-
-    useEffect(() => {
-        const localStorageGame = JSON.parse(localStorage.getItem(game.game));
-        if(!localStorageGame) return;
-        todoGame(localStorageGame);
-    }, [])
-
-    useEffect(() => {
-            localStorage.setItem(game.game, JSON.stringify(game));
-        }, [game]
-    )
-
+    
     const handleClick = () => {
-        todoGame({
-            ...game,
-            points: points + 1,
-            clicks: game.clicks + 1,
-        })
-        points = points + 1;
+        if(points >= required) { 
+            points = 0;
+            wins++;
+            required = required * 2;
+            todoGame((prevGame) => {
+                return {
+                    ...prevGame,
+                    points: points,
+                    wins: wins,
+                    required: required,
+                }
+            })
+        } else {
+            todoGame((prevGame) => {
+                return {
+                        ...prevGame,
+                        points: prevGame.points + 1,
+                        clicks: prevGame.clicks + 1,
+                    }
+                }
+            )
+            points = points + 1;
+        }
     }
 
+
+    const handleAlias = () => {
+        const aliasget = alias.current;
+        if(!aliasget) return alert('No pusiste nada');
+        console.log(aliasget)
+        todoGame((prevGame) => {
+            return {
+                ...prevGame,
+                alias: aliasget,
+            }
+        })
+    }
+
+
+    const handleClickPerSecond = () => {
+        todoGame((prevGame) => {
+                return {
+                    ...prevGame,
+                    clicksPerSecond: prevGame.seconds / prevGame.clicks,
+                    seconds: prevGame.seconds + 1,
+                }
+            }
+        )
+    }
+
+
     useEffect(() => {
+        if(!isLoad) return todoGame(() => {const localStorageGame = JSON.parse(localStorage.getItem(game.game)); return localStorageGame});
         const interval = setInterval(() => {
-            seconds++;
-            clicksPerSecond = clicks / seconds;
-            todoGame(gameObject);
+            handleClickPerSecond()
         }, 1000);
         return () => clearInterval(interval);
     }, [])
@@ -82,10 +126,16 @@ export default function Game() {
                 <h3 className='clicks_per_second'>Clicks por segundo {game.clicksPerSecond}</h3>
                 <div>
                     <h4>Escribe tu alias aqui abajo</h4>
-                    <input type="text" placeholder='Alias' ref={alias} />
+                    <input type="text" placeholder='Alias' ref={alias} /><button onClick={handleAlias}>SetAlias</button>
                 </div>
                 <button id="boton" onClick={handleClick}>Click aca!</button>
                 <div id="end_game">
+                </div>
+
+                <div id="footer">
+                    <h3>Powered by: Next.js</h3>
+                    <h4>UUID de la partida: {game.id}</h4>
+                    <h5>Tiempo en la partida: {game.seconds}</h5>
                 </div>
         </>
     );
